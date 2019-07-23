1d55e597f31555671d56a85466bffaca435d5abc
Md Shah Ali Hriody <31925919+shahalihridoy@users.noreply.github.com>
2019-07-24 00:12:22 +0600

    api key deleted

--- client/src/app/authentication/FirebaseConfig.jsx
+++ client/src/app/authentication/FirebaseConfig.jsx
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR0Y1wCk85gYmilrr-VNR5GiErhgU67LA",
-  authDomain: "shiftsimple-unity-test.firebaseapp.com",
-  databaseURL: "https://shiftsimple-unity-test.firebaseio.com",
-  projectId: "shiftsimple-unity-test",
-  storageBucket: "shiftsimple-unity-test.appspot.com",
-  messagingSenderId: "631167171614",
-  appId: "1:631167171614:web:ff435b3375973ff6"
 };
 
 firebase.initializeApp(firebaseConfig);
